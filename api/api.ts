f68a20128f7e57c7f6f8e9d8a3ba9419065d0dbc
Andrew Lai <laiandrew@hotmail.com>
2021-01-18 19:07:50 +0800

    convert to typescript

--- api/api.ts
+++ api/api.ts
@@ -1,13 +1,12 @@
-const CoinGecko = require('coingecko-api');
-
+import CoinGecko from "coingecko-api";
 //2. Initiate the CoinGecko API Client
 const CoinGeckoClient = new CoinGecko();
 
 
-const getCurrentPrice = async() => {
-  const bitcoinData = await CoinGeckoClient.coins.fetch('bitcoin', {});
-  const ethData = await CoinGeckoClient.coins.fetch('ethereum', {});
-  const dogeData = await CoinGeckoClient.coins.fetch('dogecoin', {});
+export const getCurrentPrice = async () => {
+  const bitcoinData = await CoinGeckoClient.coins.fetch('bitcoin');
+  const ethData = await CoinGeckoClient.coins.fetch('ethereum');
+  const dogeData = await CoinGeckoClient.coins.fetch('dogecoin');
 
   const btcPrice = bitcoinData.data.market_data.current_price.usd;
   const ethPrice = ethData.data.market_data.current_price.usd;
@@ -21,4 +20,3 @@ const getCurrentPrice = async() => {
   }
 
 };
-exports.getCurrentPrice = getCurrentPrice;
--- index.ts
+++ index.ts
@@ -1,14 +1,18 @@
-const readline = require('readline');
-const BigNumber = require('bignumber.js');
+import { BigNumber } from "bignumber.js";
+const readline = require("readline");
 
-const processData = (currencyRate) => {
-  let currencyList= [];
+export interface CryptoPrice {
+  [currency: string]: BigNumber.Instance
+}
+
+export const processData = (currencyRate: CryptoPrice) => {
+  let currencyList: string[] = [];
   // set round up to Round-down the result
   BigNumber.set({ ROUNDING_MODE: BigNumber.ROUND_DOWN });
 
-  return (line) => {
+  return (line: string) => {
     if (Object.keys(currencyRate).length === 0) {
-      currencyList = line.split(' ');
+      currencyList = line.split(" ");
       currencyRate = {
         BTC: new BigNumber(currencyList[0]),
         ETH: new BigNumber(currencyList[1]),
@@ -16,22 +20,25 @@ const processData = (currencyRate) => {
       };
       return;
     }
+
     const lineValues = line.split(' ');
     const saleRate = new BigNumber(lineValues[0]);
     const purchaseAmount = new BigNumber(lineValues[3]);
     const decimalPlaces = parseInt(lineValues[1]);
     const purchaseCurrency = lineValues[2];
+
     const result = saleRate
       .multipliedBy(purchaseAmount)
       .multipliedBy(currencyRate[purchaseCurrency])
       .dividedBy(currencyRate.ETH)
       .toFixed(decimalPlaces);
-    console.log(result)
+    console.log(result);
+    
     return result;
   };
 }; 
 
-const currencyRateList = {};
+const currencyRateList: CryptoPrice = {};
 const readAndOutputDetails = processData(currencyRateList);
 
 const rl = readline.createInterface({
@@ -40,4 +47,3 @@ const rl = readline.createInterface({
 
 rl.on('line', readAndOutputDetails);
 
-exports.processData = processData;
